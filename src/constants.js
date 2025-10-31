1615502ec20714f7cbc5d701b908f15c57b77196
kojesung <js95112345@gmail.com>
2025-11-01 06:25:25 +0900

    feat(CONST): 매직 넘버 상수로 정의
     가격 정보, 로또 기본 정보와 등수를 정하는 기준에 사용되는 숫자들을 상수로 정의했습니다.
    로또 가격인 1000, 1등 상금인 20000000, 1등 조건인 6개 일치의 6과 같은 숫자들은 그대로 사용했을 때 1000, 20000000, 6 이 숫자들이 각각 어떤 것을 의미하는지 알기 어렵기 때문에 변수명으로 유추할 수 있도록 했습니다.
    이와 같은 기준으로 문자열을 사용하는 경우는 따로 상수화하지 않았습니다.(이후에 필요하다고 느껴진다면 적용할 예정입니다.)

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const LOTTO = {
+  PRICE: 1000,
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  COUNT: 6,
+};
+
+export const PRIZE = {
+  FIRST: 2000000000,
+  SECOND: 30000000,
+  THIRD: 1500000,
+  FOURTH: 50000,
+  FIFTH: 5000,
+};
+
+export const MATCH_COUNT = {
+  FIRST: 6,
+  SECOND: 5,
+  THIRD: 5,
+  FOURTH: 4,
+  FIFTH: 3,
+};
