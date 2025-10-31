bc6b5319579a4a1fc33dc68e8afbfbe9602c8060
kojesung <js95112345@gmail.com>
2025-11-01 05:00:29 +0900

    feat(Output): 당첨 통계 출력 처리
    등수별 당첨 개수와 총 수익률을 출력할 수 있도록 하였습니다.

--- src/OutputView.js
+++ src/OutputView.js
@@ -10,6 +10,20 @@ class OutputView {
       Console.print(lotto);
     });
   }
+
+  static printResultStatistics(statistics) {
+    Console.print("당첨 통계");
+    Console.print("---");
+    Console.print(`3개 일치 (5,000원) - ${statistics.fifth}개`);
+    Console.print(`4개 일치 (50,000원) - ${statistics.fourth}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${statistics.third}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${statistics.second}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${statistics.first}개`);
+  }
+
+  static printRateOfReturn(rate) {
+    Console.print(`총 수익률은 ${rate}%입니다.`);
+  }
 }
 
 export default OutputView;
