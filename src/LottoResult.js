23096d11d0eeaf4b450ed811a19820c0f5d9730a
kojesung <js95112345@gmail.com>
2025-11-03 00:15:03 +0900

    feat(LottoResult): 로또 당첨 통계 및 수익률 계산 기능 구현
    구매한 모든 로또의 당첨 등급을 집계하고 총 수익률을 계산하는 클래스를 추가했습니다.
    로또의 통계와 수익률 계산 로직은 범용적으로 사용하기 어려운 계산(일반적인 계산이 아님)이기 때문에 계산 로직을 priavte 메서드로 만들고 총 수익률(getProfitRate)과 통계(getStatistics)를 확인할 수 있는 getter 메서드들만 public으로 구성하였습니다.

---
+++ src/LottoResult.js
@@ -0,0 +1,52 @@
+import LottoMatcher from "./\bLottoMatcher.js";
+import { PRIZE } from "./constants";
+
+class LottoResult {
+  #statistics = {
+    FIRST: 0,
+    SECOND: 0,
+    THIRD: 0,
+    FOURTH: 0,
+    FIFTH: 0,
+  };
+  #purchaseAmount;
+  constructor(purchaseAmount, purchasedLottos, winLotto, bonusNumber) {
+    this.#purchaseAmount = purchaseAmount;
+    this.#calculateStatics(purchasedLottos, winLotto, bonusNumber);
+  }
+  #calculateStatics(purchasedLottos, winLotto, bonusNumber) {
+    for (const purchasedLotto of purchasedLottos) {
+      const rank = LottoMatcher.getRank(purchasedLotto, winLotto, bonusNumber);
+      if (rank !== "NONE") {
+        this.#statistics[rank]++;
+      }
+    }
+  }
+
+  #getTotalPrize() {
+    return (
+      this.#statistics.FIRST * PRIZE.FIRST +
+      this.#statistics.SECOND * PRIZE.SECOND +
+      this.#statistics.THIRD * PRIZE.THIRD +
+      this.#statistics.FOURTH * PRIZE.FOURTH +
+      this.#statistics.FIFTH * PRIZE.FIFTH
+    );
+  }
+
+  getProfitRate() {
+    const totalPrize = this.#getTotalPrize();
+    const rate = (totalPrize / this.#purchaseAmount) * 100;
+    return Math.round(rate * 10) / 10;
+  }
+
+  getStatistics() {
+    return {
+      first: this.#statistics.FIRST,
+      second: this.#statistics.SECOND,
+      third: this.#statistics.THIRD,
+      fourth: this.#statistics.FOURTH,
+      fifth: this.#statistics.FIFTH,
+    };
+  }
+}
+export default LottoResult;
