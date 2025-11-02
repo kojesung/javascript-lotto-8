3f86e93767cd7c56df5811bdb1e43116f510498f
kojesung <js95112345@gmail.com>
2025-11-02 22:14:30 +0900

    feat(LottoMatcher): 당첨 등수 결정 로직 구현
    매칭되는 번호의 수와 보너스 번호 일치 여부로 등수를 결정했습니다.
    getRank라는 public 메서드로 외부에서 로또 번호와 당첨 번호, 보너스 번호를 인자로 전달해 등수를 반환하도록 했습니다.
    매 비교 시 정렬 연산을 줄이기 위해서 getSortedNumbers()라는 메서드 대신 getNumbers() 메서드를 활용했습니다.

--- src/LottoMatcher.js
+++ src/LottoMatcher.js
@@ -1,11 +1,28 @@
+import { MATCH_COUNT } from "./constants";
+
 class LottoMatcher {
+  static getRank(purchasedLotto, winLotto, bonusNumber) {
+    const matchCount = this.#getMatchCount(purchasedLotto, winLotto);
+    const hasBonus = this.#isBonusMatch(purchasedLotto, bonusNumber);
+    return this.#determineRank(matchCount, hasBonus);
+  }
+
   static #getMatchCount(purchasedLotto, winLotto) {
     const purchasedNumbers = purchasedLotto.getSortedNumbers();
     return purchasedNumbers.filter((number) => winLotto.includes(number)).length;
   }
 
   static #isBonusMatch(purchasedLotto, bonusNumber) {
-    return purchasedLotto.getSortedNumbers().includes(bonusNumber);
+    return purchasedLotto.includes(bonusNumber);
+  }
+
+  static #determineRank(matchCount, hasBonus) {
+    if (matchCount === MATCH_COUNT.FIRST) return "FIRST";
+    if (matchCount === MATCH_COUNT.SECOND && hasBonus) return "SECOND";
+    if (matchCount === MATCH_COUNT.THIRD) return "THIRD";
+    if (matchCount === MATCH_COUNT.FOURTH) return "FOURTH";
+    if (matchCount === MATCH_COUNT.FIFTH) return "FIFTH";
+    return "NONE";
   }
 }
 
