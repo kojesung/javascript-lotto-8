8d7ba6e942b486061cd5a8272ebd0e2e5b3318ed
kojesung <js95112345@gmail.com>
2025-11-03 21:18:47 +0900

    feat(LottoMatcher): 구매 로또와 당첨 로또 매칭 클래스 검증 메서드 추가
    로또의 메서드를 호출했을 때 정상적으로 불러와지는지 확인하며 전달받은 객체들이 정상적인 Lotto 객체인지 검증했습니다.
    보너스 번호가 도메인 규칙(1~45 사이)을 따르는지 검증했습니다.

--- src/LottoMatcher.js
+++ src/LottoMatcher.js
@@ -1,7 +1,8 @@
-import { MATCH_COUNT } from "./constants.js";
+import { LOTTO, MATCH_COUNT } from "./constants.js";
 
 class LottoMatcher {
   static getRank(purchasedLotto, winLotto, bonusNumber) {
+    this.#validate(purchasedLotto, winLotto, bonusNumber);
     const matchCount = this.#getMatchCount(purchasedLotto, winLotto);
     const hasBonus = this.#isBonusMatch(purchasedLotto, bonusNumber);
     return this.#determineRank(matchCount, hasBonus);
@@ -23,6 +24,17 @@ class LottoMatcher {
     if (matchCount === MATCH_COUNT.FOURTH) return "FOURTH";
     if (matchCount === MATCH_COUNT.FIFTH) return "FIFTH";
     return "NONE";
+  }
+
+  static #validate(purchasedLotto, winLotto, bonusNumber) {
+    if (!purchasedLotto || typeof purchasedLotto.getNumbers !== "function")
+      throw new Error("[ERROR] 올바른 구매 로또가 아닙니다.(정상적인 Lotto 객체가 아님)");
+
+    if (winLotto || typeof winLotto.getNumbers !== "function")
+      throw new Error("[ERROR] 올바른 당첨 로또가 아닙니다.(정상적인 Lotto 객체가 아님)");
+
+    if (typeof bonusNumber !== "number" || bonusNumber < LOTTO.MIN_NUMBER || bonusNumber > LOTTO.MAX_NUMBER)
+      throw new Error("[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.");
   }
 }
 
